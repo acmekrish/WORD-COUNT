69c1ab786cd823855f83a605517812be7dfa5a0c
Idowu Israel O.A <58216866+acmekrish@users.noreply.github.com>
2020-12-04 14:33:55 +0100

    Add files via upload

---
+++ Word_Display_and_Counter/Word_Display_and_Counter/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word_Display_and_Counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            Char[] mychar = { ' ', ',', ';', '?'};
+            string[] word = textBox1.Text.Split(mychar);
+            listBox1.Items.Clear();
+            foreach(string item in word)
+            {
+                if (item.Length > 0)
+                {
+                    listBox1.Items.Add(item);
+                    i = i + 1;
+                }
+            }
+            textBox2.Text = i.ToString();
+
+
+            
+
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            listBox1.Items.Clear();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
